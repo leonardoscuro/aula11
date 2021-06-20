385e8c4a06326370d8df962b2069fb2cd41cef3c
leonardoscuro <l178624@dac.unicamp.br>
2021-06-20 19:22:33 -0300

    commit form delete

--- js/functions.js
+++ js/functions.js
@@ -12,7 +12,7 @@ async function callFetchWithGets1(){
         headers: headers
     }
     const output = document.getElementById("json1");
-    const response = await fetch(`${url}${'database/'}`, options);
+    const response = await fetch(`${url}`, options);
 
     if (response.status >= 200 && response.status <= 300){
         console.log("Funcionou/GET");
@@ -35,7 +35,7 @@ async function callFetchWithPosts1(modelo){
         })
     }
     //await fetch(url, options);
-    await fetch(`${url}${'database/'}`, options);
+    await fetch(`${url}`, options);
 }
 
 async function callFetchWithPuts1(id, novaMensagem){
@@ -51,7 +51,7 @@ async function callFetchWithPuts1(id, novaMensagem){
         })
     }
     //await fetch(`${url}${id}`, options);
-    await fetch(`${url}${'database/'}${id}`, options);
+    await fetch(`${url}${id}`, options);
 }
 
 async function callFetchWithDeletes1(id){
@@ -64,7 +64,7 @@ async function callFetchWithDeletes1(id){
         }
     }
     //await fetch(`${url}${id}`, options);
-    await fetch(`${url}${'database/'}${id}`, options);
+    await fetch(`${url}${id}`, options);
 }
 
 
@@ -95,18 +95,157 @@ function submitDeletes1(){
     callFetchWithDeletes1(id);
     return false; // Evitar o reload da tela.
 }
-
-function submitGet(){
-    callFetchWithGet();
-    return false;
-}
 function submitGets1(){
     callFetchWithGets1();
     return false;
 }
+
+//Placa
+async function callFetchWithGets2(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+    const output = document.getElementById("json1");
+    const response = await fetch(`${url}`, options);
+
+    if (response.status >= 200 && response.status <= 300){
+        console.log("Funcionou/GET");
+        output.innerHTML = await response.text();
+    } else {
+        console.log("Deu errado");
+    }
+}
+
+async function callFetchWithPosts2(placa){
+    const options = {
+        method : 'POST',
+        mode: 'cors',
+        headers: { 
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
+        },
+        body :JSON.stringify({
+            'Placa' : placa
+        })
+    }
+    //await fetch(url, options);
+    await fetch(`${url}`, options);
+}
+
+async function callFetchWithPuts2(id, novaMensagem){
+    const options = {
+        method : 'PUT',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'            
+        }, 
+        body :JSON.stringify({
+            'Placa' : novaMensagem
+        })
+    }
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${id}`, options);
+}
+
+/*
+    Formulários
+*/
+
+function submitPosts2(){
+    console.log("entrei na função");
+    
+    const form = document.forms['postForms2'];    
+    const mensagem = form["mensagem"].value;
+    callFetchWithPosts2(mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitPuts2(){
+    const form = document.forms['putForms2'];  
+    const id = form["id"].value;  
+    const mensagem = form["mensagem"].value;
+    callFetchWithPuts2(id, mensagem);
+    return false; // Evitar o reload da tela.
+}
 function submitGets2(){
     callFetchWithGets2();
     return false;
+}
+
+//Cor
+async function callFetchWithGets3(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+    const output = document.getElementById("json1");
+    const response = await fetch(`${url}`, options);
+
+    if (response.status >= 200 && response.status <= 300){
+        console.log("Funcionou/GET");
+        output.innerHTML = await response.text();
+    } else {
+        console.log("Deu errado");
+    }
+}
+
+async function callFetchWithPosts3(cor){
+    const options = {
+        method : 'POST',
+        mode: 'cors',
+        headers: { 
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
+        },
+        body :JSON.stringify({
+            'Cor' : cor
+        })
+    }
+    //await fetch(url, options);
+    await fetch(`${url}`, options);
+}
+
+async function callFetchWithPuts3(id, novaMensagem){
+    const options = {
+        method : 'PUT',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'            
+        }, 
+        body :JSON.stringify({
+            'Cor' : novaMensagem
+        })
+    }
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${id}`, options);
+}
+
+/*
+    Formulários
+*/
+
+function submitPosts3(){
+    console.log("entrei na função");
+    
+    const form = document.forms['postForms3'];    
+    const mensagem = form["mensagem"].value;
+    callFetchWithPosts3(mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitPuts3(){
+    const form = document.forms['putForms3'];  
+    const id = form["id"].value;  
+    const mensagem = form["mensagem"].value;
+    callFetchWithPuts3(id, mensagem);
+    return false; // Evitar o reload da tela.
 }
 function submitGets3(){
     callFetchWithGets3();
